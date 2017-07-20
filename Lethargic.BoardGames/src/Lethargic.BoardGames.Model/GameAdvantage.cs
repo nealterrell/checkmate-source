1368cc28a34988b2b42e3e1469094d594385b716
Neal Terrell <neal.terrell@csulb.edu>
2017-07-19 21:05:00 -0700

    Equals method.

--- Lethargic.BoardGames/src/Lethargic.BoardGames.Model/GameAdvantage.cs
+++ Lethargic.BoardGames/src/Lethargic.BoardGames.Model/GameAdvantage.cs
@@ -3,8 +3,17 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace Lethargic.BoardGames.Model {
-	public struct GameAdvantage {
-		public int Player { get; set; }
-		public int Advantage { get; set; }
+	public struct GameAdvantage  : IEquatable<GameAdvantage> {
+		public int Player { get; private set; }
+		public int Advantage { get; private set; }
+
+		public GameAdvantage(int player, int advantage) {
+			Player = player;
+			Advantage = advantage;
+		}
+
+		public bool Equals(GameAdvantage other) {
+			return Player == other.Player && Advantage == other.Advantage;
+		}
 	}
 }
