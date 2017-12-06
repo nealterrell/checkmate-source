67638e8101f51ed5af0a50c34355fec229f45180
Neal Terrell <neal.terrell@csulb.edu>
2017-12-06 13:46:38 -0800

    Additional Othello test.

--- Part2/test/Lethargic.BoardGames.Othello.Test/ApplyMoveTests.cs
+++ Part2/test/Lethargic.BoardGames.Othello.Test/ApplyMoveTests.cs
@@ -18,7 +18,6 @@ namespace Lethargic.BoardGames.Othello.Test {
 			b.GetPlayerAtPosition(Pos(4, 4)).Should().Be(2);
 		}
 
-
 		[Fact]
 		public void OneDirectionManyFlips() {
 			OthelloBoard b = CreateBoardFromMoves(
@@ -36,6 +35,22 @@ namespace Lethargic.BoardGames.Othello.Test {
 			b.GetPlayerAtPosition(Pos(4, 2)).Should().Be(2);
 			b.GetPlayerAtPosition(Pos(4, 3)).Should().Be(2);
 			b.GetPlayerAtPosition(Pos(4, 4)).Should().Be(2);
+		}
+
+		[Fact]
+		public void ManyDirectionsOneFlip() {
+			OthelloBoard b = CreateBoardFromMoves(
+				// These moves get packaged into an OthelloMove params array by the compiler. 
+				Move(3, 2),
+				Move(4, 2)
+			);
+			Apply(b, Move(5, 2));
+
+			b.CurrentPlayer.Should().Be(2);
+			b.GetPlayerAtPosition(Pos(5, 2)).Should().Be(1);
+			b.GetPlayerAtPosition(Pos(4, 2)).Should().Be(1);
+			b.GetPlayerAtPosition(Pos(3, 2)).Should().Be(1);
+			b.GetPlayerAtPosition(Pos(4, 3)).Should().Be(1);
 		}
 	}
 }
